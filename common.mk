8a3a4d43fd25f77497d60ed2958055cfece995f0
Peter Cai <peter@typeblog.net>
2019-09-22 09:11:31 +0800

    sm8150-common: kang ims blobs from moto nash
    Change-Id: If608f4c217916fe7391034c32df9e6ae3cb0b842

--- common.mk
+++ common.mk
@@ -414,7 +414,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     telephony-ext \
     qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
 
 PRODUCT_BOOT_JARS += \
     telephony-ext

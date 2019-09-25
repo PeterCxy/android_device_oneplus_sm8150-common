db45e896c9ad0f4f91b9d90731e659a6b87cdfc9
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    sm8150-common: Convert remaining configstore settings to props
    Change-Id: Iadefa31bc863b7ab70239874bed82bef240cbba5

--- common.mk
+++ common.mk
@@ -477,3 +477,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_virtual_display_dimension=4096

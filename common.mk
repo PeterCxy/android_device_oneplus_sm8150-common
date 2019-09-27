0d42aa92d81a49f7def6652dd2c8d6321811e696
LuK1337 <priv.luk@gmail.com>
2019-09-27 13:36:07 +0200

    sm8150-common: Disable WFD for now
    Change-Id: I0c2d30a74cb9a898ccb3bac846892adf370feb8d

--- common.mk
+++ common.mk
@@ -475,8 +475,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+#PRODUCT_BOOT_JARS += \
+#    WfdCommon
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \

09a00f62f6e2a1dd65876efc0fffe64f4b0ed21f
Peter Cai <peter@typeblog.net>
2019-09-28 09:11:06 +0800

    sm8150-common: upgrade OSS audio hal
    Change-Id: Icaef2db44ce3c619e973649d9d717eeeac05640b

--- common.mk
+++ common.mk
@@ -103,9 +103,10 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl:32 \
+    android.hardware.audio@5.0-impl \
+    android.hardware.audio.effect@5.0-impl \
+    android.hardware.soundtrigger@2.1-impl \
+    android.hardware.bluetooth.audio@2.0-impl \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default \

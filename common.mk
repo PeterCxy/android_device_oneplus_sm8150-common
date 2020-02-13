bf4de7c8ae3a4f2768c316b170b2d6585c310268
AbShaheen184 <abuzar_shaheen2006@yahoo.com>
2020-02-13 21:25:14 +0300

    Enabled tri-key-state deamon

--- common.mk
+++ common.mk
@@ -471,8 +471,8 @@ PRODUCT_PACKAGES += \
 
 # tri-state key
 PRODUCT_PACKAGES += \
-    tri-state-key_daemon 
-
+    tri-state-key_daemon \
+    tri-state-key-calibrate_daemon
 
 # Trust HAL
 PRODUCT_PACKAGES += \

d0772833f4f0753a4376641fc094630689c85ff7
AbShaheen184 <abuzar_shaheen2006@yahoo.com>
2020-02-13 11:33:31 +0300

    Remove lineage touch hal and enable touch geatures for omni DeviceParts.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -230,6 +230,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.lineage.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.lineage.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
